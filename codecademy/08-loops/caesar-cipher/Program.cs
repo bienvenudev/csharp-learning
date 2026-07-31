ecd58ac50b2902dc29c6350aa0e2d0bd9c6cf512
bienvenudev <cbienvenu007@gmail.com>
2026-07-31 12:06:36 +0200

    codecademy: loops module

---
+++ codecademy/08-loops/caesar-cipher/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+      
+            Console.Write("What's your secret message?: ");
+            string secret = Console.ReadLine();
+            char[] secretMessage = secret.ToCharArray();
+            char[] encryptedMessage = new char[secretMessage.Length];
+
+            for (int i = 0; i < secretMessage.Length; i++) {
+                char character = secretMessage[i];
+                int currentCharPosition = Array.IndexOf(alphabet, character);
+
+                int keyLetterPosition = (currentCharPosition + 3) % alphabet.Length;
+
+                char encryptedChar = alphabet[keyLetterPosition];
+                encryptedMessage[i] = encryptedChar;
+            }
+      
+            Console.WriteLine(String.Join("", encryptedMessage));
+        }
+    }
+}
---
+++ codecademy/08-loops/true-or-false/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace TrueOrFalse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Do not edit these lines
+            Console.WriteLine("Welcome to 'True or False?'\nPress Enter to begin:");
+            string entry = Console.ReadLine();
+            Tools.SetUpInputStream(entry);
+
+            // Type your code below
+            string[] questions = ["is the sky blue?", "is java the same with javascript?", "is vscode better than rider?", "is college a scam?", "is smoking bad to your health?"];
+
+            bool[] answers = [true, false, true, false, true];
+            bool[] responses = new bool[answers.Length];
+
+            if (questions.Length != answers.Length) Console.WriteLine("Warning!!!");
+
+            int askingIndex = 0;
+
+            foreach (string question in questions) {
+                Console.WriteLine(question);
+                Console.WriteLine("True or false?");
+                string input = Console.ReadLine().ToLower();
+                bool isBool;
+                bool inputBool;
+
+                isBool = (input == "true" || input == "false");
+        
+                while (!isBool) {
+                    Console.WriteLine("Please respond with 'true' or 'false'.");
+                    input = Console.ReadLine().ToLower();
+                    isBool = (input == "true" || input == "false");
+                }
+
+                inputBool = input == "true";
+                responses[askingIndex] = inputBool;
+                askingIndex++;
+            }
+
+            int scoringIndex = 0;
+            int score = 0;
+
+            foreach (bool answer in answers) {
+                bool current = responses[scoringIndex];
+                Console.WriteLine($"Input: {current} | Answer:{answer}");
+                if (current == answer) score++;
+                scoringIndex++;
+            }
+
+            Console.WriteLine($"You got {score} out of {questions.Length} correct!");
+        }
+    }
+}
