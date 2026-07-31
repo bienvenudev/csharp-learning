5e139d98641c6f722bc3aa51fb73a803545848a3
bienvenudev <cbienvenu007@gmail.com>
2026-07-31 12:06:00 +0200

    codecademy: conditionals module

---
+++ codecademy/06-conditionals/choose-adventure/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ChooseYourOwnAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* THE MYSTERIOUS NOISE */
+            Console.Write("What is your name?: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}! Welcome to our story.");
+            Console.WriteLine("It begins on a cold rainy night. You're sitting in your room and hear a noise coming from down the hall. Do you go investigate?");
+
+            Console.Write("Type YES or NO: ");
+            string noiseChoice = Console.ReadLine().ToUpper();
+
+            Console.WriteLine(noiseChoice); 
+        }
+    }
+}
---
+++ codecademy/06-conditionals/password-checker/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace PasswordChecker
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int minLength = 8;
+            Console.WriteLine("Enter a password: ");
+            string input = Console.ReadLine();
+            int score = 0;
+
+            if (input.Length >= minLength) score++;
+            if (input.Any(char.IsUpper)) score++;
+            if (input.Any(char.IsLower)) score++;
+            if (input.Any(char.IsDigit)) score++;
+            bool hasSpecialChar = input.Any(ch => !char.IsLetterOrDigit(ch) && !char.IsWhiteSpace(ch));
+            if (hasSpecialChar) score++;
+      
+
+            switch(score) 
+            {
+                case 1:
+                    Console.WriteLine("Weak");
+                    break;
+                case 2:
+                    Console.WriteLine("Medium");
+                    break;
+                case 3:
+                    Console.WriteLine("Strong");
+                    break;
+                case >= 4:
+                    Console.WriteLine("Extremely strong!");
+                    break;
+                default:
+                    Console.WriteLine("Doesn't meet any standards");
+                    break;
+            }
+        }
+    }
+}
