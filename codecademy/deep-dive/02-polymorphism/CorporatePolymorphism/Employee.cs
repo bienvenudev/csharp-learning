675446ef313f9a39f122d8dc2ecfb2d6d1d365c6
bienvenudev <cbienvenu007@gmail.com>
2026-08-04 18:27:49 +0200

    Fix indentation in Employee.cs

--- codecademy/deep-dive/02-polymorphism/CorporatePolymorphism/Employee.cs
+++ codecademy/deep-dive/02-polymorphism/CorporatePolymorphism/Employee.cs
@@ -1,16 +1,16 @@
 namespace CorporatePolymorphism;
 
-    public abstract class Employee
+public abstract class Employee
+{
+    public abstract void SubmitDailyReport();
+
+    public virtual void Work()
     {
-        public abstract void SubmitDailyReport();
-
-        public virtual void Work()
-        {
-            Console.WriteLine("Employee is working.");
-        }
-
-        public void ClockIn()
-        {
-            Console.WriteLine("Employee clocks in.");
-        }
+        Console.WriteLine("Employee is working.");
     }
+
+    public void ClockIn()
+    {
+        Console.WriteLine("Employee clocks in.");
+    }
+}
