82542dd62cff94ccf698b7633b45b5eb5467fa4c
bienvenudev <cbienvenu007@gmail.com>
2026-08-19 10:33:46 +0200

    feat: add project for file

---
+++ codecademy/deep-dive/17-file/LogManager/Program.cs
@@ -0,0 +1,51 @@
+﻿class Program
+{
+    static void Main()
+    {
+        LogManager logManager = new LogManager("logs");
+        logManager.WriteLog(LogLevel.INFO, "Meeting at 6PM");
+        logManager.WriteLog(LogLevel.WARN, "Maintenance on 2nd floor");
+        logManager.WriteLog(LogLevel.ERROR, "Bug in registration system");
+
+        Console.WriteLine("---------");
+        List<LogEntry> logEntries = logManager.ReadLogEntries(logManager.currentLogPath);
+     
+        foreach (var entry in logEntries)
+        {
+            Console.WriteLine(entry);
+        }
+
+        logManager.SetMaxLogSize(100);
+     
+        for (int i = 0; i < 10; i++)
+        {
+            logManager.WriteLog(LogLevel.INFO, "Friday Pizza is being served");
+            logManager.WriteLog(LogLevel.WARN, "Maintenance today");
+            logManager.WriteLog(LogLevel.ERROR, "Bug in admin system");
+        }
+
+        //  List<FileInfo> files = logManager.ListLogFiles();
+        // foreach (FileInfo file in files)
+        // {
+        //     Console.WriteLine($"{file.Name} - {file.Length} bytes");
+        // }
+        // Console.WriteLine($"Total log files: {files.Count}");
+
+        // Get log file paths
+        List<FileInfo> files = logManager.ListLogFiles();
+        string[] logPaths = files.Select(f => f.FullName).ToArray();
+
+        // Consolidate logs into a new file
+        logManager.ConsolidateLogs(logPaths, "logs_combined.txt");
+        Console.WriteLine("Consolidation complete!");
+
+        // List files again to verify the new file exists
+        files = logManager.ListLogFiles();
+        foreach (FileInfo file in files)
+        {
+            Console.WriteLine($"{file.Name} - {file.Length} bytes");
+        }
+        Console.WriteLine($"Total log files: {files.Count}");
+
+    }
+}
---
+++ codecademy/deep-dive/17-file/LogManager/manager.cs
@@ -0,0 +1,6 @@
+namespace LogManager;
+
+public class manager
+{
+    
+}
